662d3bfd6c3622589ee7c376fa5697c94494b223
Cédric Belin <cedx@outlook.com>
2026-08-29 00:34:48 +0200

    Module merging [skip ci]

--- Sources/FunctionCoverage.ts
+++ Sources/FunctionCoverage.ts
@@ -1,5 +1,51 @@
-import type {FunctionData} from "./FunctionData.js";
 import {Tokens} from "./Tokens.js";
+
+/**
+ * Provides details for function coverage.
+ */
+export class FunctionData {
+
+	/**
+	 * The execution count.
+	 */
+	executionCount: number;
+
+	/**
+	 * The function name.
+	 */
+	functionName: string;
+
+	/**
+	 * The line number of the function start.
+	 */
+	lineNumber: number;
+
+	/**
+	 * Creates new function data.
+	 * @param options An object providing values to initialize this instance.
+	 */
+	constructor(options: FunctionDataOptions = {}) {
+		this.executionCount = Math.max(0, options.executionCount ?? 0);
+		this.functionName = options.functionName ?? "";
+		this.lineNumber = Math.max(0, options.lineNumber ?? 0);
+	}
+
+	/**
+	 * Returns a string representation of this object.
+	 * @returns The string representation of this object.
+	 */
+	toString(): string {
+		return [
+			`${Tokens.FunctionName}:${this.lineNumber},${this.functionName}`,
+			`${Tokens.FunctionData}:${this.executionCount},${this.functionName}`
+		].join("\n");
+	}
+}
+
+/**
+ * Defines the options of a {@link FunctionData} instance.
+ */
+export type FunctionDataOptions = Partial<Omit<FunctionData, "toString">>;
 
 /**
  * Provides the coverage data of functions.
